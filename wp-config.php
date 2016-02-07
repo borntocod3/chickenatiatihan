0682b2329849aa290bea771be3e5d2d86881948c
Felman Buntog <Felman Buntog>
2016-02-07 16:42:43 +0800

    added about home pages,added fancybox for galley

--- wp-config.php
+++ wp-config.php
@@ -79,6 +79,8 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+define( 'JETPACK_DEV_DEBUG', true);
+
 // define('FS_METHOD','direct');
 
 /* That's all, stop editing! Happy blogging. */
