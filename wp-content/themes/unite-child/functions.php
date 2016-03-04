55a45ad09f5f41e2865510f8ddad8bbb20fbb8c9
Jameshwart Lopez <jameshwartlopez@gmail.com>
2016-03-04 20:58:15 +0800

    Add search for id

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -188,18 +188,51 @@ function save_reservation_data(){
 add_action('wp','save_reservation_data');
 
 /**
+ * Start Custom Search
+ */
+add_filter('posts_join',function( $join ){
+    global $wpdb;
+    if( is_search() ){
+        $join.=' LEFT JOIN '.$wpdb->postmeta.' ON '.$wpdb->posts.'.ID = '.$wpdb->postmeta.'.post_id';
+    }
+    return $join;
+});
+
+add_filter('posts_where',function($where){
+    global $wpdb;
+    if ( is_search() ) {
+        $where = preg_replace(
+            "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->posts.".ID LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+    }
+
+    return $where;
+});
+add_filter('posts_distinct',function($where){
+    global $wpdb;
+
+    if ( is_search() ) {
+        return "DISTINCT";
+    }
+    return $where;
+});
+
+
+/**
+ * End Custom Search
+ */
+
+/**
  * Modify default wordpress table list for reservation post type
  * @param null
  */
 function set_reservation_custom_columns_filter( $columns ) {
-    //Add custom columns
-    $title = $columns['title'];
-    $date = $columns['date'];
     //unset default columns like title and comments
     unset($columns['comments']);
     unset($columns['title']);
     unset($columns['date']);
 
+    //Add custom columns
     $columns['ca_id']           = 'ID';
     $columns['title']           = 'Event Name';
     $columns['ca_name']         = 'Name';
